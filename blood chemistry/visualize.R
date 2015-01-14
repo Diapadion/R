cb38044e96fd674351bd446a76b31af809ccbfbe
Diapadion <diapadion@gmail.com>
2015-01-14 20:38:02 +0000

    visualization
    for real this time

--- blood chemistry/visualize.R
+++ blood chemistry/visualize.R
@@ -2,8 +2,24 @@
 library(texreg)
 
 tbl1 = htmlreg(m_1)
-texregTable(tbl1)
+print.texregTable(tbl1)
 
+tbl2a = htmlreg(m_2a)
+print.texregTable(tbl2a)
 
-tbl2c = htmlreg(c(m_2c,m2c0,m2c1,m2c2,m2c3))
-print.texregTable(tbl2c)
+tbl2b = htmlreg(m_2b)
+print.texregTable(tbl2b)
+
+#---
+
+tbl2c1 = htmlreg(m_2c)
+print.texregTable(tbl2c1)
+
+tbl2c2 = htmlreg(c(m_2c,m2c1,m2c2))
+print.texregTable(tbl2c2)
+
+tbl2c3 = htmlreg(c(m_2c,m2c1,m2c2,m2c0))
+print.texregTable(tbl2c3)
+
+tbl2c4 = htmlreg(c(m_2c,m2c1,m2c2,m2c0,m2c3))
+print.texregTable(tbl2c4)

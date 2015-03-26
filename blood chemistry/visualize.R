eac00b6de4068c2c54b5f9c7aace4c2489c47826
Diapadion <diapadion@gmail.com>
2015-03-26 22:44:25 +0000

    confint plots
    at last

--- blood chemistry/visualize.R
+++ blood chemistry/visualize.R
@@ -282,11 +282,126 @@ install.packages("coefplot2",
 
 #####
 
-cchol.preds = confint(m2.chol)
+#cchol.preds = confint(m2.chol)
+
+sample_vect = c((rep(c('Japanese'),6)),(rep(c('Americans'),6)),(rep(c('Yerkes'),6)))
+
+pd <- position_dodge(width=0.2,height=NULL)
+
+# Systolic CF plots
+c.sys.co=fixef(m1a)[2:7]
+m.sys.co=m.sys$coefficients[5:10]
+j.sys.co=mj.sys$coefficients[5:10]
+
+c.sys.prd = confint(m1a)[4:9,]
+m.sys.prd = confint(m.sys)[5:10,]
+j.sys.prd = confint(mj.sys)[5:10,]
+
+sys.ci = data.frame(
+  Coefficients=c(j.sys.co,m.sys.co,c.sys.co),rbind(j.sys.prd,m.sys.prd,c.sys.prd),sample_vect,Dimension=rep(rownames(m.sys.prd),3)
+    )
+colnames(sys.ci)<-c("Coefficients",'lower',"upper","Sample","Dimension")
+
+sys.cf.gg = ggplot(sys.ci, aes(Sample,Coefficients, color=Sample)) +
+  geom_point(aes(shape=Sample),size=4, position=pd) + 
+#  scale_color_manual(name="Sample",values=c("coral","steelblue")) + 
+  theme_bw() + 
+#  scale_x_continuous("Sample", breaks=1:length(Sample), labels=Sample) + 
+  scale_y_continuous("Regression coefficients")   + 
+  geom_errorbar(aes(ymin=lower,ymax=upper),width=0.1,position=pd) + facet_wrap(~ Dimension, ncol=2)
 
 
-m1a$
-m.sys$coefficients[1]
-m.sys.preds = confint(m.sys)
+# Diastolic CF plots
+c.dias.co=fixef(m1b)[2:7]
+m.dias.co=m.dias$coefficients[5:10]
+j.dias.co=mj.dias$coefficients[5:10]
+
+c.dias.prd = confint(m1b)[4:9,]
+m.dias.prd = confint(m.dias)[5:10,]
+j.dias.prd = confint(mj.dias)[5:10,]
+
+dias.ci = data.frame(
+  Coefficients=c(j.dias.co,m.dias.co,c.dias.co),rbind(j.dias.prd,m.dias.prd,c.dias.prd),sample_vect,Dimension=rep(rownames(m.dias.prd),3)
+)
+colnames(dias.ci)<-c("Coefficients",'lower',"upper","Sample","Dimension")
+
+dias.cf.gg <- ggplot(dias.ci, aes(Sample,Coefficients, color=Sample)) +
+  geom_point(aes(shape=Sample),size=4, position=pd) + 
+  #  scale_color_manual(name="Sample",values=c("coral","steelblue")) + 
+  theme_bw() + 
+  #  scale_x_continuous("Sample", breaks=1:length(Sample), labels=Sample) + 
+  scale_y_continuous("Regression coefficients")   + 
+  geom_errorbar(aes(ymin=lower,ymax=upper),width=0.1,position=pd) + facet_wrap(~ Dimension, ncol=2)
 
 
+# Triglyceride CF plots
+c.trig.co=fixef(m2.trig)[2:7]
+m.trig.co=m.trig$coefficients[5:10]
+j.trig.co=mj.trig$coefficients[5:10]
+
+c.trig.prd = confint(m2.trig)[4:9,]
+m.trig.prd = confint(m.trig)[5:10,]
+j.trig.prd = confint(mj.trig)[5:10,]
+
+trig.ci = data.frame(
+  Coefficients=c(j.trig.co,m.trig.co,c.trig.co),rbind(j.trig.prd,m.trig.prd,c.trig.prd),sample_vect,Dimension=rep(rownames(m.trig.prd),3)
+)
+colnames(trig.ci)<-c("Coefficients",'lower',"upper","Sample","Dimension")
+
+trig.cf.gg <- ggplot(trig.ci, aes(Sample,Coefficients, color=Sample)) +
+  geom_point(aes(shape=Sample),size=4, position=pd) + 
+  #  scale_color_manual(name="Sample",values=c("coral","steelblue")) + 
+  theme_bw() + 
+  #  scale_x_continuous("Sample", breaks=1:length(Sample), labels=Sample) + 
+  scale_y_continuous("Regression coefficients")   + 
+  geom_errorbar(aes(ymin=lower,ymax=upper),width=0.1,position=pd) + facet_wrap(~ Dimension, ncol=2)
+
+
+# Cholesterol CF plots
+c.chol.co=fixef(m2.chol)[2:7]
+m.chol.co=m.chol$coefficients[5:10]
+j.chol.co=mj.chol$coefficients[5:10]
+
+c.chol.prd = confint(m2.chol)[4:9,]
+m.chol.prd = confint(m.chol)[5:10,]
+j.chol.prd = confint(mj.chol)[5:10,]
+
+chol.ci = data.frame(
+  Coefficients=c(j.chol.co,m.chol.co,c.chol.co),rbind(j.chol.prd,m.chol.prd,c.chol.prd),sample_vect,Dimension=rep(rownames(m.chol.prd),3)
+)
+colnames(chol.ci)<-c("Coefficients",'lower',"upper","Sample","Dimension")
+
+chol.cf.gg <- ggplot(chol.ci, aes(Sample,Coefficients, color=Sample)) +
+  geom_point(aes(shape=Sample),size=4, position=pd) + 
+  #  scale_color_manual(name="Sample",values=c("coral","steelblue")) + 
+  theme_bw() + 
+  #  scale_x_continuous("Sample", breaks=1:length(Sample), labels=Sample) + 
+  scale_y_continuous("Regression coefficients")   + 
+  geom_errorbar(aes(ymin=lower,ymax=upper),width=0.1,position=pd) + facet_wrap(~ Dimension, ncol=2)
+
+
+# Creatinine CF plots
+c.creat.co=fixef(m2.creat)[2:7]
+m.creat.co=m.creat$coefficients[5:10]
+j.creat.co=mj.creat$coefficients[5:10]
+
+c.creat.prd = confint(m2.creat)[4:9,]
+m.creat.prd = confint(m.creat)[5:10,]
+j.creat.prd = confint(mj.creat)[5:10,]
+
+creat.ci = data.frame(
+  Coefficients=c(j.creat.co,m.creat.co,c.creat.co),rbind(j.creat.prd,m.creat.prd,c.creat.prd),sample_vect,Dimension=rep(rownames(m.creat.prd),3)
+)
+colnames(creat.ci)<-c("Coefficients",'lower',"upper","Sample","Dimension")
+
+creat.cf.gg <- ggplot(creat.ci, aes(Sample,Coefficients, color=Sample)) +
+  geom_point(aes(shape=Sample),size=4, position=pd) + 
+  #  scale_color_manual(name="Sample",values=c("coral","steelblue")) + 
+  theme_bw() + 
+  #  scale_x_continuous("Sample", breaks=1:length(Sample), labels=Sample) + 
+  scale_y_continuous("Regression coefficients")   + 
+  geom_errorbar(aes(ymin=lower,ymax=upper),width=0.1,position=pd) + facet_wrap(~ Dimension, ncol=2)
+
+ggsave(filename = 'creat.png', plot = creat.cf.gg, 
+       scale = 1, width = 7, height = 10,
+       dpi = 300, limitsize = TRUE)

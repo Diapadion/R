6c5b7121f23b0771f3dbf4dd3aa154116479b32c
Diapadion <diapadion@gmail.com>
2016-12-07 14:41:44 +0000

    pre-Vanessa chat

---
+++ fWHR/chimps.R
@@ -0,0 +1,37 @@
+####### Chimp faces #######
+
+### TO MEASURE:
+#
+# - Yerkes
+# - Bastrop?
+# - sub-sample of Japan & Edi chimps
+#
+# > sample(c(2:260), 20)
+# [1]  14  54 224  77 191 177 240  21   8 139  15 162 100  40  75 145 182  87 114  93
+#
+# Holland - Edith 1.JPG
+# kilimi oct2013b.JPG
+# Pal.20060611
+# 2013-11-27 14.56.34
+# Konatsu.20030702
+# Gon.20070620
+# Shiomi.20030410
+# 2013-11-27 15.12.02-1.jpg
+# David P1010077.tem
+# 2013-11-27 13.57.01
+# Holland - Paul (left) and Edith (right).JPG
+# Cleo.20060611霊研屋外 024
+# Edinburgh - Lucy 2.JPG
+# Holland - Frek 1.JPG
+# 2013-11-27 14.22.24-1
+# 2013-11-27 14.37.34-1
+# James20041102
+# Liberius.JPG
+# Louis P1020290.JPG
+# 2013-11-27 14.12.13
+
+
+
+
+
+

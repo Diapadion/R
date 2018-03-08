8d044239dc7c071796df294dfe2ac80e3aa3fbdc
Diapadion <diapadion@gmail.com>
2018-03-08 16:36:44 +0000

    pre-Burn
    all sorts of stuff

--- fWHR/chimp_fWHRmodels.R
+++ fWHR/chimp_fWHRmodels.R
@@ -22,19 +22,19 @@ set.seed(1234567)
 chFP = read.csv('chimpFacesPersDemos.csv')
 chFP = chFP[,-1]
 
-colnames(chFP)
-dim(chFP)
+# colnames(chFP)
+# dim(chFP)
 chFP = chFP[!is.na(chFP$fWHR),]
-dim(chFP)
+# dim(chFP)
 
 
 
 ### Descriptive Statistics
 
-levels(chFP$ID)
-table(droplevels(chFP$ID[chFP$location=='Edinburgh']))
-sum(table(droplevels(chFP$ID[chFP$location=='Bastrop'])))
-table(droplevels(chFP$ID[chFP$location=='Japan']))
+#levels(chFP$ID)
+table(droplevels(chFP$ID[chFP$location=='Edinburgh'])) # Number of Edi chimps and images/chimp
+sum(table(droplevels(chFP$ID[chFP$location=='Bastrop']))) # Number of Bastrop chimps
+table(droplevels(chFP$ID[chFP$location=='Japan'])) # NUmber of Japanese chimps and images/
 
 table(chFP$Sex[!duplicated(chFP$ID)]) # Number of males and females
 
@@ -43,13 +43,13 @@ sd(chFP$Age[!duplicated(chFP$ID)], na.rm=TRUE) # SD age
 min(chFP$Age[!duplicated(chFP$ID)], na.rm=TRUE) # Min age
 max(chFP$Age[!duplicated(chFP$ID)], na.rm=TRUE) # Max age
 
-mean(table(chFP$ID))
-sd(table(chFP$ID))
+mean(table(chFP$ID)) # Mean number of useable images
+sd(table(chFP$ID))   # SD of number of useable images
 
 
 ### Zero-order correlation plots
 
-colnames(chFP)
+#colnames(chFP)
 corrplot(cor(chFP[,c(8,12,14,15,16,17,18,19),],use="pairwise.complete.obs")
          ,  method = 'number')
 
@@ -61,7 +61,8 @@ corrplot(cor(chFP[chFP$Sex==1,c(8,12,14,15,16,17,18,19),],use="pairwise.complete
 ### Mixed Effect Models
 
 ## Collapse subspecies random effects
-levels(chFP$Subspecies)
+
+#levels(chFP$Subspecies)
 chFP$Subspecies[chFP$Subspecies=='hybrid'] = 'unknown'
 table(chFP$Subspecies[!duplicated(chFP$ID)])
 
@@ -76,23 +77,16 @@ table(chFP$Sex[!duplicated(chFP$ID)&chFP$Subspecies=='unknown'])
 # basic age,sex index:
 adults = (chFP$Sex==0 & chFP$Age>7) | (chFP$Sex==1 & chFP$Age>9)
 adults[is.na(adults)] = TRUE
-
-count(adults)
-
 adults = adults & (as.character(chFP$ID) != 'Gage') # 75
-
-count(adults)
-
 adults = adults & (as.character(chFP$ID) != 'Lennon') # This is a good place to pull out Lennon (130, 131)
-
-count(adults)
-
-adults
+# count(adults)
+# View(chFP[adults,])
 
 
 
 ## Base: just subspecies random effects
 
+set.seed(1234567)
 m0 <- lmer(fWHR ~ 1 +
              (1 | location) + (1 | Subspecies) + (1 | ID:Subspecies)
            ,data=chFP[adults,]
@@ -114,6 +108,7 @@ ranef(m0.0)
 
 ## Age and Age squared, cubed
 
+set.seed(1234567)
 m1 <- lmer(fWHR ~ s(Age) + s(I(Age^2)) + s(I(Age^3)) +
              (1 | location) + (1 | Subspecies) + (1 | ID:Subspecies)
            ,data = chFP[adults,]
@@ -128,6 +123,7 @@ confint(m1, method='profile')
 
 
 
+set.seed(1234567)
 m2 <- lmer(fWHR ~ Age*Sex +
              (1 | location) + (1 | Subspecies) + (1 | ID:Subspecies)
            ,data = chFP[adults,]
@@ -135,12 +131,13 @@ m2 <- lmer(fWHR ~ Age*Sex +
 summary(m2)
 confint(m2, method='profile')
 
-# Again, nothing. Yet more difficult to keep including these.
+# Again, nothing. Yet more difficult to include these.
 
 
 
 ### Personality
 
+set.seed(1234567)
 mp1 <- lmer(fWHR ~ Dom_CZ + Ext_CZ + Con_CZ + Agr_CZ + Neu_CZ + Opn_CZ +
               (1 | location) + (1 | Subspecies) + (1 | ID:Subspecies)
             ,data = chFP[adults,]
@@ -155,16 +152,15 @@ confint(mp1, method='profile')
 
 ## Split by sex
 
+set.seed(1234567)
 mp2.f <- lmer(fWHR ~ Dom_CZ + Ext_CZ + Con_CZ + Agr_CZ + Neu_CZ + Opn_CZ +
                 (1 | location) + (1 | Subspecies) + (1 | ID:Subspecies)
               ,data = chFP[adults&chFP$Sex==0,]
 )
-
 mp2.m <- lmer(fWHR ~ Dom_CZ + Ext_CZ + Con_CZ + Agr_CZ + Neu_CZ + Opn_CZ +
                 (1 | location) + (1 | ID:Subspecies) + (1 | Subspecies)
               ,data = chFP[adults&chFP$Sex==1,]
 )
-
 
 summary(mp2.f)
 summary(mp2.m)
@@ -180,34 +176,21 @@ count(chFP$Subspecies)
 table(chFP$Subspecies,chFP$Sex)
 
 set.seed(1234567)
-
 mp3.t = lmer(fWHR ~ Dom_CZ + Ext_CZ + Con_CZ + Agr_CZ + Neu_CZ + Opn_CZ +
                (1 | location) + 
              (1 | ID)
              ,data = chFP[adults&chFP$Subspecies=='troglodytes',]
-             #,data = chFP[adults&chFP$Subspecies=='troglodytes'&chFP$Sex==1,]
 )
-
-
-
 mp3.v = lmer(fWHR ~ Dom_CZ + Ext_CZ + Con_CZ + Agr_CZ + Neu_CZ + Opn_CZ +
                (1 | location) + (1 | ID)
              #,data = chFP[chFP$Subspecies=='verus',]
              ,data = chFP[adults&chFP$Subspecies=='verus',]
 )
 
-# # Worth doing for unknowns?
-# mp3.u = lmer(fWHR ~ Dom_CZ + Ext_CZ + Con_CZ + Agr_CZ + Neu_CZ + Opn_CZ +
-#                (1 | location) + (1 | ID)
-#              ,data = chFP[is.na(chFP$Subspecies),])
-# summary(mp3.u) # Nothing.
-
 summary(mp3.t) # Nothing for troglodytes.
 summary(mp3.v)
 
-set.seed(1234567)
-
-confint(mp3.t, method='Wald')
+confint(mp3.t, method='Wald') # Profile not optimal
 confint(mp3.v, method='profile')
 
 # In Verus, low C and low N (which are neg corr'd) are both neg associated with fWHR
@@ -217,7 +200,6 @@ confint(mp3.v, method='profile')
 ## Verus is the only subspecies with reasonable sample size
 
 set.seed(1234567)
-
 mp4.f = lmer(fWHR ~ Dom_CZ + Ext_CZ + Con_CZ + Agr_CZ + Neu_CZ + Opn_CZ +
                (1 | location) + (1 | ID)
              ,data = chFP[indx&(chFP$Subspecies=='verus'&chFP$Sex==0),]
@@ -239,60 +221,37 @@ mp4.m = lmer(fWHR ~ Dom_CZ + Ext_CZ + Con_CZ + Agr_CZ + Neu_CZ + Opn_CZ +
 )
 
 summary(mp4.f)
-
 confint(mp4.f, method='profile')
-# 2.5 %     97.5 %
-#   .sig01       0.00000000 0.14967947
-# .sig02       0.00000000 0.34803668
-# .sigma       0.17818178 0.26753845
-# (Intercept)  1.61955158 1.91370410
-# Dom_CZ       0.00817667 0.20753487
-# Ext_CZ      -0.13630212 0.12185953
-# Con_CZ      -0.14912258 0.07709833
-# Agr_CZ      -0.06656844 0.19159615
-# Neu_CZ      -0.12393911 0.07226640
-# Opn_CZ      -0.09946781 0.07316191
-#confint(mp4.f, method='boot')
-
 
 summary(mp4.m)
-
 confint(mp4.m, method='profile')
-# 2.5 %      97.5 %
-#   .sig01       0.07779975  0.25612958
-# .sig02       0.06246343  0.62575035
-# .sigma       0.14322512  0.22881822
-# (Intercept)  1.33656500  2.03737732
-# Dom_CZ      -0.33445053 -0.01900332
-# Ext_CZ      -0.45372506  0.18310760
-# Con_CZ      -0.24671679  0.21099174
-# Agr_CZ      -0.18221122  0.19102970
-# Neu_CZ      -0.29580105 -0.01959169
-# Opn_CZ      -0.08888618  0.35837230
-#confint(mp4.m, method='boot')
+
+
+
+
 
 
 
 # Trying different optimizers for females model
 
-afurl <- "https://raw.githubusercontent.com/lme4/lme4/master/inst/utils/allFit.R"
-eval(parse(text=getURL(afurl)))
-
-aa <- allFit(mp4.f)
-is.OK <- sapply(aa,is,"merMod")
-aa.OK <- aa[is.OK]
-lapply(aa.OK,function(x) x@optinfo$conv$lme4$messages)
-(lliks <- sort(sapply(aa.OK,logLik)))
-# Any of these seem like they would work in theory
-
-mp4.f.2 <- update(mp4.f,start=ss,control=lmerControl(optimizer="Nelder_Mead"#,
-                                                     #optCtrl=list(maxiter=2e5)
-))
-summary(mp4.f)
-confint(mp4.f.2, method='profile')
-confint(mp4.f.2, method='boot')
-# Nelder-Mead seems to work the best
-# Has been reincorporated above.
+# afurl <- "https://raw.githubusercontent.com/lme4/lme4/master/inst/utils/allFit.R"
+# eval(parse(text=getURL(afurl)))
+# 
+# aa <- allFit(mp4.f)
+# is.OK <- sapply(aa,is,"merMod")
+# aa.OK <- aa[is.OK]
+# lapply(aa.OK,function(x) x@optinfo$conv$lme4$messages)
+# (lliks <- sort(sapply(aa.OK,logLik)))
+# # Any of these seem like they would work in theory
+# 
+# mp4.f.2 <- update(mp4.f,start=ss,control=lmerControl(optimizer="Nelder_Mead"#,
+#                                                      #optCtrl=list(maxiter=2e5)
+# ))
+# summary(mp4.f)
+# confint(mp4.f.2, method='profile')
+# confint(mp4.f.2, method='boot')
+# # Nelder-Mead seems to work the best
+# # Has been reincorporated above.
 
 
 

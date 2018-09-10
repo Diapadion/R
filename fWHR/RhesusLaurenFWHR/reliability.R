d5add8c06b7e4428577c154e81f74cde7f29c9b7
Diapadion <diapadion@gmail.com>
2018-09-10 21:08:40 +0100

    home clean up - fWHR

--- fWHR/RhesusLaurenFWHR/reliability.R
+++ fWHR/RhesusLaurenFWHR/reliability.R
@@ -98,17 +98,55 @@ alpha(reliabl[,c("LFFH.a","LFFH.b")])
 View(reliabl)
 
 ## distance between points
-## - nothing to correlate distances with
 ## - average distance and CI per point
 
 a.ed = apply
 euc.dist(reliabl$a.x,reliabl$a.y,reliabl$A.x,reliabl$A.y)
-a.ed = apply(reliabl[,c(2:3,17,18)],MARGIN=1,FUN=euc.dist)
+
+for(i in 1:dim(reliabl)[1]){
+  reliabl$a.ed[i] = euc.dist(reliabl$a.x[i],reliabl$a.y[i],reliabl$A.x[i],reliabl$A.y[i])
+  reliabl$b.ed[i] = euc.dist(reliabl$b.x[i],reliabl$b.y[i],reliabl$B.x[i],reliabl$B.y[i])
+  reliabl$c.ed[i] = euc.dist(reliabl$c.x[i],reliabl$c.y[i],reliabl$C.x[i],reliabl$C.y[i])
+  reliabl$d.ed[i] = euc.dist(reliabl$d.x[i],reliabl$d.y[i],reliabl$D.x[i],reliabl$D.y[i])
+  reliabl$e.ed[i] = euc.dist(reliabl$e.x[i],reliabl$e.y[i],reliabl$E.x[i],reliabl$E.y[i])
+  reliabl$f.ed[i] = euc.dist(reliabl$f.x[i],reliabl$f.y[i],reliabl$F.x[i],reliabl$F.y[i])
+  reliabl$g.ed[i] = euc.dist(reliabl$g.x[i],reliabl$g.y[i],reliabl$G.x[i],reliabl$G.y[i])
+}
+
+summary(reliabl$a.ed)
+
+reliabl[10,]
+reliabl[4,]
+
+rlbl <- reliabl[-c(4,10),]
+
+mean(rlbl$a.ed)
+median(rlbl$a.ed)
+mean(rlbl$a.ed)+c(-1.96,1.96)*sd(rlbl$a.ed)/sqrt(length(rlbl$a.ed))
+
+mean(rlbl$b.ed)
+median(rlbl$b.ed)
+mean(rlbl$b.ed)+c(-1.96,1.96)*sd(rlbl$b.ed)/sqrt(length(rlbl$b.ed))
+
+mean(rlbl$c.ed)
+median(rlbl$c.ed)
+mean(rlbl$c.ed)+c(-1.96,1.96)*sd(rlbl$c.ed)/sqrt(length(rlbl$c.ed))
+
+mean(rlbl$d.ed)
+median(rlbl$d.ed)
+mean(rlbl$d.ed)+c(-1.96,1.96)*sd(rlbl$d.ed)/sqrt(length(rlbl$d.ed))
+
+mean(rlbl$e.ed)
+median(rlbl$e.ed)
+mean(rlbl$e.ed)+c(-1.96,1.96)*sd(rlbl$e.ed)/sqrt(length(rlbl$e.ed))
+
+mean(rlbl$f.ed)
+median(rlbl$f.ed)
+mean(rlbl$f.ed)+c(-1.96,1.96)*sd(rlbl$f.ed)/sqrt(length(rlbl$f.ed))
+
+mean(rlbl$g.ed)
+median(rlbl$g.ed)
+mean(rlbl$g.ed)+c(-1.96,1.96)*sd(rlbl$g.ed)/sqrt(length(rlbl$g.ed))
 
 
-
-## correlation of coordinates?
-
-
-### TODO
 

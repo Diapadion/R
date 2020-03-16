0a9a85bf9a72d3613a44d3a744e59b2fdbe3705f
Drew Altschul <dmaltschul@gmail.com>
2020-03-16 10:29:28 +0000

    COVID for home
    Last intended day at the office - 16/03/20

---
+++ MIDUSdom-ExplConf/discrimValid.R
@@ -0,0 +1,7 @@
+### Discriminant validity
+
+# https://link.springer.com/article/10.1007/s11747-014-0403-8
+# https://link.springer.com/article/10.1007/s11747-015-0455-4#Ack1
+
+library(semTools)
+?htmt
